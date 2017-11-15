c20d3276f45f560f646ed140a5038d338f9a135b
Andrey Oleynik <andrew-archer@mail.ru>
2017-11-16 01:39:42 +0300

    added google test

--- Makefile
+++ Makefile
@@ -5,21 +5,23 @@ CXXFLAGS = -Wall -std=c++14
 
 CXXFLAGS_DEBUG = $(CXXFLAGS) -g
 OBJDIR_DEBUG = ./Debug/x64
-OUT_DEBUG = ../x64/t1_d
+OUT_DEBUG = ./x64/t1_d
 
 CXXFLAGS_RELEASE = $(CXXFLAGS) -O2
 OBJDIR_RELEASE = ./Release/x64
-OUT_RELEASE = ../x64/t1
+OUT_RELEASE = ./x64/t1
 
 OBJ_DEBUG = $(OBJDIR_DEBUG)/main.o
 OBJ_RELEASE = $(OBJDIR_RELEASE)/main.o
+
+LIBS = -lgtest -lgmock -lpthread
 
 all: debug release
 
 clean: clean_debug clean_release
 
 before_debug: 
-	test -d ../x64 || mkdir -p ../x64
+	test -d ./x64 || mkdir -p ./x64
 	test -d $(OBJDIR_DEBUG) || mkdir -p $(OBJDIR_DEBUG)
 
 after_debug: 
@@ -27,18 +29,18 @@ after_debug:
 debug: before_debug out_debug after_debug
 
 out_debug: before_debug $(OBJ_DEBUG)
-	$(LD) -o $(OUT_DEBUG) $(OBJ_DEBUG)
+	$(LD) -o $(OUT_DEBUG) $(OBJ_DEBUG) $(LIBS)
 
 $(OBJDIR_DEBUG)/main.o: main.cpp
 	$(CXX) $(CXXFLAGS_DEBUG) -c main.cpp -o $(OBJDIR_DEBUG)/main.o
 
 clean_debug: 
 	rm -f $(OBJ_DEBUG) $(OUT_DEBUG)
-	rm -rf ../x64
+	rm -rf ./x64
 	rm -rf $(OBJDIR_DEBUG)
 
 before_release: 
-	test -d ../x64 || mkdir -p ../x64
+	test -d ./x64 || mkdir -p ./x64
 	test -d $(OBJDIR_RELEASE) || mkdir -p $(OBJDIR_RELEASE)
 
 after_release: 
@@ -46,7 +48,7 @@ after_release:
 release: before_release out_release after_release
 
 out_release: before_release $(OBJ_RELEASE)
-	$(LD) -o $(OUT_RELEASE) $(OBJ_RELEASE)
+	$(LD) -o $(OUT_RELEASE) $(OBJ_RELEASE) $(LIBS)
 
 $(OBJDIR_RELEASE)/main.o: main.cpp
 	$(CXX) $(CXXFLAGS_RELEASE) -c main.cpp -o $(OBJDIR_RELEASE)/main.o
--- main.cpp
+++ main.cpp
@@ -1,28 +1,59 @@
 #include "robot_commands.h"
 #include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 //-----------------------------------------------------------------------------
 
 
-class DummyRobot : public IRobot {
+class MockRobot : public IRobot {
 public:
-  void Move(double distance) override {};
-  void Turn(double angle) override {};
-  void Beep() override {};
+    MOCK_METHOD0(Beep, void());
+    MOCK_METHOD1(Move, void(double distance));
+    MOCK_METHOD1(Turn, void(double angle));
 };
 
 
 
-int main() {
+TEST(RobotCommands, CommandsAreSent) {
   RobotCommands rcs;
-  // commands entered by an operator
+  double move_val = 125.4878, turn_val = -0.15784;
   rcs.AddCommand(CommandId::BEEP);
   rcs.AddCommand(CommandId::BEEP);
-  rcs.AddCommand(CommandId::MOVE, 125.4878);
-  rcs.AddCommand(CommandId::TURN, -0.15784);
+  rcs.AddCommand(CommandId::MOVE, move_val);
+  rcs.AddCommand(CommandId::TURN, turn_val);
+  rcs.AddCommand(CommandId::BEEP);
 
-  DummyRobot r1, r2;
-  rcs.SendTo(&r1); // send commands to robot r1
-  rcs.SendTo(&r2); // send commands to robot r2
+  // send commands to robot r1
+  MockRobot r1;
+  EXPECT_CALL(r1, Beep())
+  .Times(3);
+  EXPECT_CALL(r1, Move(move_val))
+  .Times(1);
+  EXPECT_CALL(r1, Turn(turn_val))
+  .Times(1);
+  rcs.SendTo(&r1);
 
-  return 0;
+  // send the same saved commands to robots r2 and r3
+  MockRobot r2, r3;
+  EXPECT_CALL(r2, Beep())
+  .Times(3);
+  EXPECT_CALL(r2, Move(move_val))
+  .Times(1);
+  EXPECT_CALL(r2, Turn(turn_val))
+  .Times(1);
+  EXPECT_CALL(r3, Beep())
+  .Times(3);
+  EXPECT_CALL(r3, Move(move_val))
+  .Times(1);
+  EXPECT_CALL(r3, Turn(turn_val))
+  .Times(1);
+  rcs.SendTo(&r2);
+  rcs.SendTo(&r3);
+}
+
+
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
